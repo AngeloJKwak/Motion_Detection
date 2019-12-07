aef4799a89606cf98f4e6588c0e351ede8e6a323
AngeloJKwak <angelokwak@gmail.com>
2019-12-06 21:45:49 -0500

    uploaded wrong file in last commit

--- motion_detection.m
+++ motion_detection.m
@@ -2,14 +2,7 @@ motion_detection_project();
 %main function that will run all 4 algorithms
 function motion_detection_project()
 frame_folders = ['ArenaA','ArenaN','AShipDeck','getin','getout','movecam','trees','walk'];
-%here we will call run_algorithms in a loop, iterating through each
-%directory in the frame_folders. After getting the results, it will the end
-%of the loop will take the result matrix and export it as the 4 panel image
-%needed for that frame for the video
 
-%send directory to func
-%for i = 1:size(frame_folders)
-%run_algorithms
 run_algorithms('ArenaA');
 end
 
@@ -19,6 +12,7 @@ vid_frames = dir(fullfile(frame_dir, '*.jpg'));
 n_files = length(vid_frames); %get the number of frames
 
 grayed_frames = cell(n_files,1); %init the cell array to hold the frames
+
 for frame=1:length(vid_frames)
     file_name = vid_frames(frame).name;
     %Get the full file path for the current frame
@@ -31,43 +25,6 @@ end
 frames = grayed_frames;
 end
 
-%Function to get the backgrounds needed for simple frame differencing
-function simp_frame_diff_backgrounds = get_simp_frame_diff_backgrounds(frame_dir,grayed_frames)
-vid_frames = dir(fullfile(frame_dir, '*.jpg'));
-n_files = length(vid_frames); %get the number of frames
-simp_frame_diff_backs = cell(n_files,1);
-simp_frame_diff_backs{1} = grayed_frames{1};
-
-for frame=2:length(vid_frames)
-    %calculate the background images for each frame for simple frame diff
-    simp_frame_diff_backs{frame} = grayed_frames{frame-1};
-end
-simp_frame_diff_backgrounds = simp_frame_diff_backs;
-end
-
-%Function to get the backgrounds needed for adaptive background
-%substitution
-function adaptive_back_sub_backgrounds = get_adaptive_back_sub_backgrounds(frame_dir,grayed_frames,alpha)
-vid_frames = dir(fullfile(frame_dir, '*.jpg'));
-n_files = length(vid_frames); %get the number of frames
-adaptive_back_sub_backs = cell(n_files,1);
-adaptive_back_sub_backs{1} = grayed_frames{1};
-
-for frame=2:length(vid_frames)
-    %calculate the background images for each frame for simple frame diff
-    adaptive_back_sub_backs{frame} = alpha*grayed_frames{frame} + (1-alpha)*adaptive_back_sub_backs{frame-1};
-end
-adaptive_back_sub_backgrounds = adaptive_back_sub_backs;
-end
-
-%Function to get the backgrounds needed for persistent frame differencing
-function persistent_frame_diff_backgrounds = get_persistent_frame_diff_backgrounds(frame_dir)
-vid_frames = dir(fullfile(frame_dir, '*.jpg'));
-n_files = length(vid_frames); %get the number of frames
-persistent_frame_diff_backgrounds = cell(n_files,1);
-
-end
-
 function final = run_algorithms(frame_dir)
 %this function will go through a directory frame by frame
 %it will read in every frame from the directory, and pass the frame to each
@@ -77,54 +34,71 @@ vid_frames = dir(fullfile(frame_dir, '*.jpg'));
 n_files = length(vid_frames); %get the number of frames
 
 grayed_frames = load_and_convert(frame_dir);
-init_background_frame = grayed_frames{1};
-%Get backgrounds for simple frame differencing
-simp_frame_diff_backs = get_simp_frame_diff_backgrounds(frame_dir, grayed_frames);
-%Get backgrounds for adaptive background substitution
-adaptive_back_sub_backgrounds = get_adaptive_back_sub_backgrounds(frame_dir,grayed_frames,0.5);
+
+M_simple_sub = simple_background_subtraction(grayed_frames,60);
+M_simple_diff = simple_frame_differencing(grayed_frames,60);
+M_adaptive_background = adaptive_background_subtraction(grayed_frames,0.5,60);
+M_persistent_frame_diff = persistent_frame_differencing(grayed_frames, 50, 60);
 
 for fr = 2:size(grayed_frames,1)
     file_name = vid_frames(fr).name;
-    
-    current_frame = grayed_frames{fr};
-    simp_diff_frame_back = simp_frame_diff_backs{fr};
-    adaptive_back_sub_back = adaptive_back_sub_backgrounds{fr};
-   
-    %Run simple background subtraction
-    M_simple_sub = simple_background_subtraction(current_frame,init_background_frame,50);
-    %Run simple frame differencing
-    M_simple_diff = simple_frame_differencing(current_frame, simp_diff_frame_back, 50);
-    %Run adaptive background subtraction
-    M_adaptive_background = adaptive_background_subtraction(current_frame, adaptive_back_sub_back, 50);
-
-    final = [M_simple_sub,M_simple_diff,M_adaptive_background];%,M_adaptive_background];
-    new_file = strcat('new_walk',file_name); %This needs to be fixed to dynamically output the new frame images based on the current directory
-    imwrite(final, new_file);
+    final = [M_simple_sub{fr},M_simple_diff{fr};M_adaptive_background{fr},M_persistent_frame_diff{fr}];
+    new_file = strcat('TEST_walk',file_name); %This needs to be fixed to dynamically output the new frame images based on the current directory
+    imwrite(final, new_file);  
 end
 
 end
 
 
-function M_subtract = simple_background_subtraction(current_frame, base, threshold)
-    diff = abs(base - current_frame);
-    M_subtract = diff > threshold;
+function M_subtract = simple_background_subtraction(video_frames,threshold)
+M_sub_frames = cell(length(video_frames),1);
+base_frame = video_frames{1};
+
+for frame=2:length(video_frames)
+    diff = abs(base_frame - video_frames{frame});
+    M_sub_frames{frame} = diff > threshold;
+end 
+M_subtract = M_sub_frames;
 end
-%function for simple frame differencing
-%This is working, however some back edges are not showing... consider maybe
-%playing with this by doing the grayscaling the way he has it in bobtips
+
 %pdf
-function M_diff = simple_frame_differencing(current_frame, background, threshold)
-    diff = abs(background - current_frame);
-    M_diff = diff > threshold;
+function M_diff = simple_frame_differencing(video_frames,threshold)
+M_diff_frames = cell(length(video_frames),1);
+backgrounds = cell(length(video_frames),1);
+backgrounds{1} = video_frames{1};
+for frame=2:length(video_frames)
+    diff = abs(backgrounds{frame-1} - video_frames{frame});
+    M_diff_frames{frame} = diff > threshold;
+    backgrounds{frame} = video_frames{frame};
+end 
+M_diff = M_diff_frames;
 end
-%function for adaptive background subtraction
-function M_adapt = adaptive_background_subtraction(g_current_frame, g_prev_frame, threshold)
-    diff = abs(g_prev_frame - g_current_frame);
-    M_adapt = (diff > threshold);
+
+function M_adapt = adaptive_background_subtraction(video_frames,alpha,threshold)
+M_adaptive_frames = cell(length(video_frames),1);
+backgrounds = cell(length(video_frames),1);
+backgrounds{1} = video_frames{1};
+
+for frame=2:length(video_frames)
+    diff = abs(backgrounds{frame-1} - video_frames{frame});
+    M_adaptive_frames{frame} = diff > threshold;
+    backgrounds{frame} = alpha*video_frames{frame} + (1-alpha)*backgrounds{frame-1};
+end 
+M_adapt = M_adaptive_frames;
 end
-%function for persistent frame differencing
-function M_persist = persistent_frame_differencing(frame, threshold, gamma_val)
 
+function M_persist = persistent_frame_differencing(video_frames, gamma, threshold)
+M_persist_frames = cell(length(video_frames),1);
+backgrounds = cell(length(video_frames),1);
+backgrounds{1} = video_frames{1};
+h_vals = cell(length(video_frames),1);
 
-
+for frame=2:length(video_frames)
+    diff = abs(backgrounds{frame-1} - video_frames{frame});
+    M_persist_frames{frame} = diff > threshold;
+    temp = max(h_vals{frame-1}-gamma, 0);
+    h_vals{frame} = max(255*M_persist_frames{frame}, temp);
+    backgrounds{frame} = video_frames{frame};
+end
+M_persist = M_persist_frames;
 end
